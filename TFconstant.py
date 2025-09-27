ed125e95c0e244a53bde5ce9ac2408a3cd24f7a3
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2025-09-27 12:05:04 +0530

    Update TFconstant.py
    Creating a constant tensor

--- TFconstant.py
+++ TFconstant.py
@@ -5,6 +5,9 @@ s1 = tf.constant(7)
 
 # Print the tensor
 print("Tensor:", s1)
+#Tensor: tf.Tensor(7, shape=(), dtype=int32)
+
 
 # Print the number of dimensions
 print("Number of dimensions:", s1.ndim)
+#Number of dimensions: 0
